71d0f34b60efc2dbb1a34ea68722fb62f7c860e4
Andrew Walling <twalling1689@gmail.com>
2026-01-21 13:01:05 -0600

    Assignment 2.3
    1. Write a console application to create a text file and save your basic details like name, age, address ( use dummy data). Read the details from same file and print on console.

---
+++ Assignment2.3_IOData/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment2._3_IOData
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+
+        public new StringBuilder ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("".PadLeft(120, '-'));
+            sb.Append($"\nName: {Name}\tAge: {Age}\t\tAddress: {Address}\n");
+            sb.Append("".PadLeft(120,'-'));
+            return sb;
+        }
+    }
+}
---
+++ Assignment2.3_IOData/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace Assignment2._3_IOData
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string filePath = path + @"\PersonalData.txt";
+
+
+            StreamWriter streamWriter = null;
+
+            Person person = new Person();
+
+            Console.Write("What is your name: ");
+            person.Name = Console.ReadLine();
+            Console.Write("\nAge: ");
+            person.Age = int.Parse(Console.ReadLine());
+            Console.WriteLine("\nAddress: ");
+            person.Address = Console.ReadLine();
+
+
+            try
+            {
+                if (!File.Exists(filePath))
+                {
+                    streamWriter = File.CreateText(filePath);
+                    streamWriter.WriteLine(person.ToString().ToString());
+                    Console.WriteLine("File created and written to");
+                }
+                else
+                {
+                    File.AppendAllText(filePath, person.ToString().ToString());
+                    Console.WriteLine("File already exists...Appended to file");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally { if(streamWriter != null) streamWriter.Close(); } // Can not access file from machine if you forget to close this
+
+
+            //----------------------------------Read File-----------------------------------------------
+            using (StreamReader streamReader = new StreamReader(filePath))
+            {
+                string contentRead = streamReader.ReadToEnd();
+                Console.WriteLine($"This was pulled using the 'using' block\n{contentRead}");
+            }
+
+            Console.WriteLine("\n\nThis was pulled using a while loop without the 'using' block");
+            StreamReader streamReader2 = new StreamReader(filePath);
+
+            string line;
+            while((line = streamReader2.ReadLine()) != null)
+                Console.WriteLine(line);
+            streamReader2.Close();
+
+        }
+    }
+}
